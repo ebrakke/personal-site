2ae9ee7a9e96e4263170ba0ef8d4ba3efa7d096b
Erik Brakke <erik@datasembly.com>
2021-01-08 07:00:31 -0700

    initial commit

---
+++ src/components/Nav.svelte
@@ -0,0 +1,68 @@
+<script lang="ts">
+	export let segment: string;
+</script>
+
+<style>
+	nav {
+		border-bottom: 1px solid rgba(255,62,0,0.1);
+		font-weight: 300;
+		padding: 0 1em;
+		display: flex;
+		align-items: center;
+		background-color: hsl(190, 77%, 30%);
+		color: white;
+	}
+
+	ul {
+		margin: 0;
+		padding: 0;
+	}
+
+	/* clearfix */
+	ul::after {
+		content: '';
+		display: block;
+		clear: both;
+	}
+
+	li {
+		display: block;
+		float: left;
+	}
+
+	[aria-current] {
+		position: relative;
+		display: inline-block;
+	}
+
+	[aria-current]::after {
+		position: absolute;
+		content: '';
+		width: calc(100% - 1em);
+		height: 2px;
+		background-color: rgb(255,62,0);
+		display: block;
+		bottom: -1px;
+	}
+
+	a {
+		text-decoration: none;
+		padding: 1em 0.5em;
+		display: block;
+		color: white;
+	}
+
+	.brand {
+		margin: 0 auto;
+	}
+</style>
+
+<nav>
+	<ul>
+		<li><a aria-current="{segment === undefined ? 'page' : undefined}" href=".">home</a></li>
+		<li><a aria-current="{segment === 'about' ? 'page' : undefined}" href="about">about</a></li>
+	</ul>
+	<div class="brand">
+		<h2><a href=".">Erik Brakke Web Design</a></h2>
+	</div>
+</nav>
---
+++ src/routes/_layout.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import Nav from '../components/Nav.svelte';
+
+	export let segment: string;
+</script>
+
+<style>
+	main {
+		margin-top: 16px;
+		padding: 24px;
+		background-color: hsl(190, 77%, 98%);
+		height: 100vh;
+	}
+</style>
+
+<Nav {segment}/>
+
+<main class="container is-fluid">
+	<slot></slot>
+</main>
